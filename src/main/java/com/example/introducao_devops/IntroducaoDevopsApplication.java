5711bfd65b0555a3496a2914042fb8bbda364b0a
Pedro Henrique <pedrohenriquedasilvapereira12@gmail.com>
2025-09-17 20:15:04 -0300

    Hello World

---
+++ src/main/java/com/example/introducao_devops/IntroducaoDevopsApplication.java
@@ -0,0 +1,19 @@
+package com.example.introducao_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class IntroducaoDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroducaoDevopsApplication.class, args);
+	}
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+}
